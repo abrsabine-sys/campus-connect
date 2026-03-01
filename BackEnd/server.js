9ec801d226b0bbda7101a9c2efe9f729409eb777
abrsabine-sys <abr.sabine@gmail.com>
2026-03-01 18:53:09 -0500

    fixing deployment issues

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -21,4 +21,6 @@ app.use('/events', eventRoutes);
 app.use(express.static(path.join(__dirname, '../Public')));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
